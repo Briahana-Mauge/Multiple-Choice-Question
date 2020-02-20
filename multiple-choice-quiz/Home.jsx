f22fd62807a06cd9fb3894e01ae3fa31ed36d4a3
Bpmauge <briahanamauge@pursuit.org>
2020-02-19 21:38:10 -0500

    added home component

---
+++ multiple-choice-quiz/Home.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+const Home = () => {
+  return(
+    <div>
+    <h1>Hello, World!</h1>
+    <p>Come test your web development skills with our Quizzes. </p>
+    <p><em>Good Luck!</em></p>
+    </div>
+  )
+}
+export default Home;
