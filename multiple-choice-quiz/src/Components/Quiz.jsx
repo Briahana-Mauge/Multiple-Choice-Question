9ffdc8e3e79b815f0a735b2389893cf2d5b3a328
Bpmauge <briahanamauge@pursuit.org>
2020-02-21 23:02:09 -0500

    create drop down for quiz selection

--- multiple-choice-quiz/src/Components/Quiz.jsx
+++ multiple-choice-quiz/src/Components/Quiz.jsx
@@ -3,41 +3,65 @@ import { quizzes } from '../quizzes';
 import MultipleChoiceQuiz from './MultipleChoiceQuiz';
 
 class Quiz extends React.Component {
-  constructor() {
-    super();
-    this.state = {
-      quizIndex: 0,
-    };
-  }
-
-  changeQuiz = () => {
-    const { quizIndex } = this.state;
-    if (quizIndex === quizzes.length - 1) {
-      let newIndex = 0;
-      this.setState({
-        quizIndex: newIndex,
-      });
-    } else {
-      let newIndex = quizIndex + 1;
-      this.setState({
-        quizIndex: newIndex,
-      });
+    constructor() {
+        super();
+        this.state = {
+            quizIndex: 0,
+            quizOption: []
+        };
     }
-  };
-  
-  render() {
-    const { quizIndex } = this.state;
-    return (
-      <div>
-        <MultipleChoiceQuiz
-          quiz={quizzes[quizIndex]}
-          changeQuiz={this.changeQuiz}
-        />
-        <div className = 'hello'>
-        </div>
-      </div>
-    );
-  }
+    componentDidMount() {
+        this.populateSelect();
+    }
+    populateSelect = () => {
+        let newQuizzes = [];
+        for (let i = 0; i < quizzes.length; i++) {
+            newQuizzes.push(
+                <option value={quizzes[i].id} key={quizzes[i].id}>{quizzes[i].title}</option>
+            )
+        }
+
+        this.setState({
+            quizOption: newQuizzes
+        })
+    }
+    changeQuiz = () => {
+        const { quizIndex } = this.state;
+        if (quizIndex === quizzes.length - 1) {
+            let newIndex = 0;
+            this.setState({
+                quizIndex: newIndex,
+            });
+        } else {
+            let newIndex = quizIndex + 1;
+            this.setState({
+                quizIndex: newIndex,
+            });
+        }
+    };
+    handleInput = (event) => {
+        const { name, value } = event.target
+        this.setState({
+            [name]: value,
+           
+        })
+    }
+    render() {
+        const { quizIndex, quizOption } = this.state;
+        return (
+            <div>
+                <select className='quiz' name='quizIndex' onChange={this.handleInput}>
+                    {quizOption}
+                </select>
+                <MultipleChoiceQuiz
+                    quiz={quizzes[quizIndex]}
+                    changeQuiz={this.changeQuiz}
+                />
+                <div className='hello'>
+                </div>
+            </div>
+        );
+    }
 }
 
 export default Quiz;
